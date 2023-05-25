7bdc0f21e67763c9428a0dbb609c904cfb46c657
Gabo <cristhiancarmona32@aragon.unam.mx>
2023-05-25 13:26:21 -0600

    ActExtra8

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,30 @@
 package org.example;
 
+import org.example.controlador.ControladorLibro;
 import org.example.modelo.Libro;
 import org.example.persistencia.DemoLibroDB;
 import org.example.persistencia.LibroDAO;
+import org.example.vista.VentanaLibro;
 
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        LibroDAO ldao = new LibroDAO();
+        VentanaLibro view = new VentanaLibro("MVC y JDBC");
+        ControladorLibro controller = new ControladorLibro(view);
 
 
-        try {
+        // LibroDAO ldao = new LibroDAO();
+
+
+
+
+
+
+
+
+
+        /*try {
            Libro res = (Libro) ldao.buscarPorId("1");
             System.out.println(res);
             System.out.println("-----------------------------------");
@@ -22,7 +35,7 @@ public class Main {
         }catch (SQLException sqle){
             System.out.println("Error ");
             System.out.println(sqle.getMessage());
-        }
+        }*/
 
 
 
